ce941f455461ff402c7833cb086baef33011c55b
Patrick Krabeepetcharat <patrickk@Patricks-MacBook-Pro.local>
2015-12-23 11:02:34 -0800

    just had to change the import structure. Module works now.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,8 +1,9 @@
 import $ from 'jquery';
 
-import init from './modules/test.js';
+import {init} from './modules/test.js';
 
 $(document).ready(function(){
+	console.log('ready');
 	document.body.innerHTML += '<div>Hello World</div>';
 	init();
 });
