584979d0f619215cb986d0e2bf7d5d304fcb101e
Patrick Krabeepetcharat <patrickk@Patricks-MacBook-Pro.local>
2015-12-24 20:53:16 -0800

    changed some startup template stuff

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,6 +4,5 @@ import {init} from './modules/test.js';
 
 $(document).ready(function(){
 	console.log('ready');
-	document.body.innerHTML += '<div>Hello World</div>';
 	init();
 });
--- src/scripts/modules/test.js
+++ src/scripts/modules/test.js
@@ -1,3 +1,3 @@
 export function init() {
-	console.log('test module success');
+	console.log('test module init');
 }
