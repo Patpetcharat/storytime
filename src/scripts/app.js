54b2652ec88a81061a8dda43fa63ba3a1511a90a
Patrick Krabeepetcharat <patrickk@Patricks-MacBook-Pro.local>
2015-12-27 22:57:51 -0800

    Add React

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,6 @@
 import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 import {init} from './modules/test.js';
 
@@ -6,3 +8,8 @@ $(document).ready(function(){
 	console.log('ready');
 	init();
 });
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('cards')
+);
